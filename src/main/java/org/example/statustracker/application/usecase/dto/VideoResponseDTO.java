bf5f8e34beb555a8dc68be3edd00d82606ef5511
Diógenes Viana <65191714+diogenesviana@users.noreply.github.com>
2025-01-09 16:56:16 -0300

    docs: Anotações no dto para o swagger.

--- src/main/java/org/example/statustracker/application/usecase/dto/VideoResponseDTO.java
+++ src/main/java/org/example/statustracker/application/usecase/dto/VideoResponseDTO.java
@@ -1,6 +1,7 @@
 package org.example.statustracker.application.usecase.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import org.example.statustracker.domain.entity.VideoStatus;
 
@@ -10,18 +11,23 @@ import java.util.UUID;
 public class VideoResponseDTO {
 
     @JsonProperty("id")
+    @Schema(example = "123e4567-e89b-12d3-a456-426614174000")
     private String id;
 
     @JsonProperty("url")
+    @Schema(example = "https://www.youtube.com/watch?v=123456")
     private String url;
 
     @JsonProperty("userName")
+    @Schema(example = "user123")
     private String userName;
 
     @JsonProperty("userMail")
+    @Schema(example = "user@email.com")
     private String userMail;
 
     @JsonProperty("status")
+    @Schema(example = "EM_PROCESSAMENTO")
     private VideoStatus status;
 
     public VideoResponseDTO(String id, String url, String userName, String userMail, VideoStatus status) {
